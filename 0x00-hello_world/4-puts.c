e9b13ff68f4ccfe557c2771351631bfadc16c24d
wasetor <torsolbaba3@gmail.com>
2022-12-08 20:51:48 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-/*
+/**
  * main - print a line of code using the put function
- *
  * return: 0
-*/
+**/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
