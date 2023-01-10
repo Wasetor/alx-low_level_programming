742c1538e780e337b1b5719fe58caa179130d679
wasetor <torsolbaba3@gmail.com>
2023-01-10 11:11:06 -0800

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
 
@@ -10,7 +9,7 @@
  * Return: nothing
  */
 
-void free_grid(int **grid, int height)
+ivoid free_grid(int **grid, int height)
 {
 	int i;
 
