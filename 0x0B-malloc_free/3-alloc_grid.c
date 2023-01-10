d557c713997bae2e8bf77a31ace0d4461e8c9dcb
wasetor <torsolbaba3@gmail.com>
2023-01-10 11:05:21 -0800

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
+
 /**
  * free_grid - frees 2d array
  * @grid: 2d grid
@@ -8,6 +9,7 @@
  * Description: frees memory of grid
  * Return: nothing
  */
+
 void free_grid(int **grid, int height)
 {
 	int i;
